5ba8678ff0e58738daeb3dd2c4df1288716d9f5b
Jeremy T. Braun <jtbraun@squidevil.net>
2012-02-10 20:50:27 -0800

    Bumped version to 1.967006 for non-development release.

--- lib/Parse/RecDescent.pm
+++ lib/Parse/RecDescent.pm
@@ -1895,7 +1895,7 @@ use vars qw ( $AUTOLOAD $VERSION $_FILENAME);
 
 my $ERRORS = 0;
 
-our $VERSION = '1.967_005';
+our $VERSION = '1.967006';
 $VERSION = eval $VERSION;
 $_FILENAME=__FILE__;
 
@@ -3377,7 +3377,7 @@ Parse::RecDescent - Generate Recursive-Descent Parsers
 
 =head1 VERSION
 
-This document describes version 1.967_005 of Parse::RecDescent
+This document describes version 1.967006 of Parse::RecDescent
 released January 29th, 2012.
 
 =head1 SYNOPSIS
