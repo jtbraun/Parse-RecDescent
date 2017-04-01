9208a8d093305e48ba7371a247a4e75e827bd97f
Jeremy T. Braun <jtbraun@squidevil.net>
2017-04-01 10:18:58 -0700

    Provide repository and bugtracker entries in MYMETA.* (#110403)
    Provide repository and bugtracker entries in MYMETA.* (RT.cpan.org
    
    Resources are shared between Build.PL and Makefile.PL via MYMETA.pl.

---
+++ MYMETA.pl
@@ -0,0 +1,10 @@
+resources => {
+    repository => {
+        type => 'git',
+        url => 'git://github.com/jtbraun/Parse-RecDescent',
+        web => 'https://github.com/jtbraun/Parse-RecDescent',
+    },
+    bugtracker => {
+        web => 'https://rt.cpan.org/Dist/Display.html?Status=Active&Queue=Parse-RecDescent',
+    },
+},
